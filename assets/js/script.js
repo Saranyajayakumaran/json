333d917708046b58585b5597be4029f9bbcda50c
Saranya <jsaran.bj@gmail.com>
2024-04-10 15:08:11 +0200

    get request and errror checking in json

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+const API_KEY = "96K3oZps6d53X71rHjgaKouWFY8123";//ApI key
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+    else{
+        throw new Error(data.error);
+
+    }
+
+}
